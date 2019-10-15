bae53ae7b7240123f7ddfa1cc08acebc5c78bd0c
Mark Orimoloye <>
2019-10-15 15:19:10 +0200

    Hands-on Java with Caleb Curry.  Day-1

---
+++ day1/src/day1/Day1.java
@@ -0,0 +1,28 @@
+/*
+ * Programming with Caleb Curry. Day 1
+ */
+package day1;
+
+import java.util.Scanner;
+
+/*
+ * @author Mark Orimoloye
+ */
+public class Day1 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        System.out.println("Hello World~!");
+        
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        
+        System.out.println(s);
+    }
+    
+}
